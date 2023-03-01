1ad275e652a331e9bdd69df18f282cbd1c880168
alexey.e <alexey.e@core-tech.ru>
2023-03-01 23:15:45 +0300

    The functionality of the converter has been completed, the ability to take data from a json file and add it to the database has been added. Added checks and feature descriptions. Added test file with questions.

--- ask_me_bot/questions/exceptions.py
+++ ask_me_bot/questions/exceptions.py
@@ -4,3 +4,23 @@ class DataExportError(Exception):
 
 class JsonKeysError(Exception):
     pass
+
+
+class ThemeNotExistedError(Exception):
+    pass
+
+
+class QuestionLengthError(Exception):
+    pass
+
+
+class ExplanationLengthError(Exception):
+    pass
+
+
+class AnswerLengthError(Exception):
+    pass
+
+
+class LotIncorrectAnswersError(Exception):
+    pass
