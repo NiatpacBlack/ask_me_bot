b175fb0b7b4cc1efbad9d90c9ff393d8ea179edf
NeMmiddle <SteamEGTS322@yandex.ru>
2023-03-02 23:33:00 +0300

    Added the ability to pass a blitz

--- ask_me_bot/bot_app.py
+++ ask_me_bot/bot_app.py
@@ -1,6 +1,3 @@
-import time
-
-import telebot
 from telebot import TeleBot, types
 
 from questions.services import get_question_and_answers
@@ -28,13 +25,13 @@ def start(message) -> None:
 def blic_or_without_timer(message):
     """Depending on the user's choice, sends him either a quiz with or without time"""
     if message.text == 'Викторина без времени':
-        quiz_without_timer(message)
+        quiz(message)
     elif message.text == 'Блиц':
-        quiz_with_timer_runs_until_the_first_wrong_answer(message)
+        quiz_with_timer(message)
 
 
 @bot.message_handler(content_types=['text'])
-def quiz_without_timer(message: types.Message) -> None:
+def quiz(message: types.Message) -> None:
     """Forms from sends a quiz without time"""
     data = get_question_and_answers()
     if message.text == 'Викторина без времени':
@@ -45,10 +42,11 @@ def quiz_without_timer(message: types.Message) -> None:
                       correct_option_id=data[2],
                       explanation=data[3],
                       is_anonymous=False,
-                      )
+        )
 
 
-def quiz_with_timer_runs_until_the_first_wrong_answer(message):
+
+def quiz_with_timer(message):
     """Forms and sends a quiz with a timer"""
     global data
     data = get_question_and_answers()
@@ -60,22 +58,23 @@ def quiz_with_timer_runs_until_the_first_wrong_answer(message):
                   explanation=data[3],
                   open_period=15,
                   is_anonymous=False
-                  ).message_id
+        )
 
     @bot.poll_answer_handler()
     def handle_poll_answer(pollAnswer):  # pollAnswer stores user data and his answer to QUIZ
-        """We fix the selected user's answer in the BLIT mode.
+        """We fix the selected user's answer in the BLITZ mode.
            If the answer is correct, we immediately send him the next QUIZ.
            And so on until the first wrong answer"""
         if int(pollAnswer.option_ids[0]) == data[2]: # Compare the selected user answer with the correct answer from the database
             global correct_answer_counter
             correct_answer_counter += 1
-            quiz_with_timer_runs_until_the_first_wrong_answer(message)
+            quiz_with_timer(message)
         else:
             print('else')
             bot.send_message(
             message.from_user.id,
-            text=f"В этот раз {pollAnswer.user.username} у тебя {correct_answer_counter} правильных ответов подряд")
+            text=f"В этот раз {pollAnswer.user.username} у тебя {correct_answer_counter} правильных ответов подряд"
+            )
 
             # Unfinished block:
             # It is necessary to make it so that after the completion of the blitz, the user is redirected to the main menu
