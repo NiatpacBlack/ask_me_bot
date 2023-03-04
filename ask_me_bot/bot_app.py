d534002300f65c216105b22dbeeb4d836468bd83
NeMmiddle <SteamEGTS322@yandex.ru>
2023-03-04 21:08:56 +0300

    The ability to pass the blitz until the first wrong answer

--- ask_me_bot/bot_app.py
+++ ask_me_bot/bot_app.py
@@ -1,89 +1,115 @@
+import time
+from datetime import datetime
+
 from telebot import TeleBot, types
 
 from questions.services import get_question_and_answers
 from config import BOT_TOKEN
 
-
 bot = TeleBot(BOT_TOKEN)
 
-correct_answer_counter = 0
+total_points = 0
+
+correct_user_responses = []
+all_user_responses = []
 
 
-@bot.message_handler(commands=["start"])
-def start(message) -> None:
+@bot.message_handler(commands=["/"])
+def start(message: types.Message) -> None:
     """Displays a welcome message and start menu to the user."""
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     btn1 = types.KeyboardButton("Викторина без времени")
     btn2 = types.KeyboardButton("Блиц")
     markup.add(btn1, btn2)
-    bot.send_message(
+    msg = bot.send_message(
         message.chat.id,
-        text=f"Здравствуйте, {message.from_user.full_name}, выберите действие:", reply_markup=markup)
-    bot.clear_step_handler(message)
+        text=f"Здравствуйте, {message.from_user.full_name}, выберите действие:",
+        reply_markup=markup,
+    )
+    bot.register_next_step_handler(msg, blic_or_without_timer)
 
 
-@bot.message_handler(content_types=['text'])
-def blic_or_without_timer(message):
+def blic_or_without_timer(message: types.Message) -> None:
     """Depending on the user's choice, sends him either a quiz with or without time"""
-    if message.text == 'Викторина без времени':
+    if message.text == "Викторина без времени":
         quiz(message)
-    elif message.text == 'Блиц':
+    elif message.text == "Блиц":
         quiz_with_timer(message)
 
 
-@bot.message_handler(content_types=['text'])
 def quiz(message: types.Message) -> None:
     """Forms from sends a quiz without time"""
     data = get_question_and_answers()
-    if message.text == 'Викторина без времени':
-        bot.send_poll(message.chat.id,
-                      type='quiz',
-                      question=data[0],
-                      options=data[1],
-                      correct_option_id=data[2],
-                      explanation=data[3],
-                      is_anonymous=False,
-                      )
+    msg = bot.send_poll(
+        message.chat.id,
+        type="quiz",
+        question=data[0],
+        options=data[1],
+        correct_option_id=data[2],
+        explanation=data[3],
+        is_anonymous=False,
+    )
+    bot.register_next_step_handler(msg, blic_or_without_timer)
 
 
-def quiz_with_timer(message):
+def quiz_with_timer(message: types.Message) -> None:
     """Forms and sends a quiz with a timer"""
-    global data
+    global total_points
+    total_points += 1
     data = get_question_and_answers()
-    bot.send_poll(message.chat.id,
-                  type='quiz',
-                  question=data[0],
-                  options=data[1],
-                  correct_option_id=data[2],
-                  explanation=data[3],
-                  open_period=15,
-                  is_anonymous=False
-                  )
-
-    @bot.poll_answer_handler()
-    def handle_poll_answer(pollAnswer):  # pollAnswer stores user data and his answer to QUIZ
-        """We fix the selected user's answer in the BLITZ mode.
-           If the answer is correct, we immediately send him the next QUIZ.
-           And so on until the first wrong answer"""
-        if int(pollAnswer.option_ids[0]) == data[
-            2]:  # Compare the selected user answer with the correct answer from the database
-            global correct_answer_counter
-            correct_answer_counter += 1
-            quiz_with_timer(message)
+    msg = bot.send_poll(
+        message.chat.id,
+        type="quiz",
+        question=data[0],
+        options=data[1],
+        correct_option_id=data[2],
+        explanation=data[3],
+        open_period=5,
+        is_anonymous=False,
+    )
+    time_send = datetime.now().second
+    time_end = time_send + 5
+    while True:
+        if time_send < time_end:
+            while True:
+                if total_responses(len(all_user_responses)) != total_points and time_send <= time_end:
+                    time_send += 1
+                    time.sleep(1)
+                else:
+                    while True:
+                        if all_user_responses != [] and all_user_responses[-1] == data[2]:
+                            correct_user_responses.append(all_user_responses[-1])
+                            quiz_with_timer(message)
+                        else:
+                            bot.send_message(
+                                message.chat.id,
+                                text=f"В этот раз у тебя {len(correct_user_responses)} "
+                                     f"правильных ответов подряд! \nЗнай, ты всегда можешь испытать себя снова!",
+                            )
+                            correct_user_responses.clear()
+                            all_user_responses.clear()
+                            total_points = 0
+                            bot.register_next_step_handler(msg, blic_or_without_timer)
+                        break
+                    break
         else:
-            print('else')
             bot.send_message(
-                message.from_user.id,
-                text=f"В этот раз {pollAnswer.user.username} у тебя {correct_answer_counter} правильных ответов подряд"
+                message.chat.id,
+                text=f"Старайся успевать отвечать до того, как кончится время.\n "
             )
-
-            # Unfinished block:
-            # It is necessary to make it so that after the completion of the blitz, the user is redirected to the main menu
-
-            # Possible Methods:
-            # bot.stop_poll()
-            # bot.delete_message()
+        break
 
 
-if __name__ == '__main__':
+def total_responses(*args):
+    total = len(all_user_responses)
+    return total
+
+
+@bot.poll_answer_handler()
+def handle_poll_answer(poll_answer) -> None:
+    all_user_responses.append(int(poll_answer.option_ids[0]))
+    pass
+
+
+if __name__ == "__main__":
     bot.polling()
