0b5852ffd4e0356a126fee47e02ca2bc7207f331
NeMmiddle <SteamEGTS322@yandex.ru>
2023-03-05 22:04:47 +0300

    Fixed a bug with incorrect output to the blitz if you do not have time to answer before the end of the timer

--- ask_me_bot/bot_app.py
+++ ask_me_bot/bot_app.py
@@ -39,6 +39,8 @@ def blic_or_without_timer(message: types.Message) -> None:
 
 def quiz(message: types.Message) -> None:
     """Forms from sends a quiz without time"""
+    global total_points
+    total_points += 1
     data = get_question_and_answers()
     msg = bot.send_poll(
         message.chat.id,
@@ -68,45 +70,50 @@ def quiz_with_timer(message: types.Message) -> None:
         is_anonymous=False,
     )
     time_send = datetime.now().second
-    time_end = time_send + 5
+    time_end = time_send + 4.5
     while True:
-        if time_send < time_end:
+        if total_responses(len(all_user_responses)) != total_points and time_send < time_end:
+            time_send += 1
+            time.sleep(1)
+        elif time_send < time_end:
             while True:
-                if total_responses(len(all_user_responses)) != total_points and time_send <= time_end:
-                    time_send += 1
-                    time.sleep(1)
+                if all_user_responses != [] and all_user_responses[-1] == data.index_current_answer:
+                    correct_user_responses.append(all_user_responses[-1])
+                    quiz_with_timer(message)
                 else:
-                    while True:
-                        if all_user_responses != [] and all_user_responses[-1] == data[2]:
-                            correct_user_responses.append(all_user_responses[-1])
-                            quiz_with_timer(message)
-                        else:
-                            bot.send_message(
-                                message.chat.id,
-                                text=f"В этот раз у тебя {len(correct_user_responses)} "
-                                     f"правильных ответов подряд! \nЗнай, ты всегда можешь испытать себя снова!",
-                            )
-                            correct_user_responses.clear()
-                            all_user_responses.clear()
-                            total_points = 0
-                            bot.register_next_step_handler(msg, blic_or_without_timer)
-                        break
-                    break
+                    bot.send_message(
+                        message.chat.id,
+                        text=f"В этот раз у тебя {len(correct_user_responses)} "
+                             f"правильных ответов подряд! \nЗнай, ты всегда можешь испытать себя снова!",
+                    )
+                    correct_user_responses.clear()
+                    all_user_responses.clear()
+                    total_points = 0
+                    bot.register_next_step_handler(msg, blic_or_without_timer)
+                break
+            break
         else:
             bot.send_message(
                 message.chat.id,
                 text=f"Старайся успевать отвечать до того, как кончится время.\n "
+                     f"У тебя {len(correct_user_responses)} правильных ответов из {len(all_user_responses)}"
             )
-        break
+            correct_user_responses.clear()
+            all_user_responses.clear()
+            total_points = 0
+            bot.register_next_step_handler(msg, blic_or_without_timer)
+            break
 
 
 def total_responses(*args):
+    """Used in quiz_with_timer to update the quiz response counter"""
     total = len(all_user_responses)
     return total
 
 
 @bot.poll_answer_handler()
 def handle_poll_answer(poll_answer) -> None:
+    """Handler for handling survey responses"""
     all_user_responses.append(int(poll_answer.option_ids[0]))
     pass
 
